d9b70f613177eb2c2ec3034435cc803f52043618
unknown <alan07vb@gmail.com>
2021-06-07 04:30:35 -0300

    alterando versão descrição

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -21,7 +21,7 @@ public class BootcampApplication {
 	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
 		return new OpenAPI().info(new Info()
 				.title(description)
-				.version("3.0")
+				.version("2.1")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
